9a1f5a5e777bb9d5c42838fbec39ee616829baec
bella meng <dancingm1998@gmail.com>
2021-04-23 16:11:42 +0800

    print tx hash

--- analysis/index_addresses.hpp
+++ analysis/index_addresses.hpp
@@ -1,2 +1,3 @@
 
+
 void index_addresses(int , const char **);
---
+++ analysis/main.cpp
@@ -0,0 +1,11 @@
+#include <blocksci/blocksci.hpp>
+#include "index_addresses.hpp"
+
+using namespace blocksci;
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    index_addresses(argc, argv);
+    return 0;
+}
