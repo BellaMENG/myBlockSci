05543306f51fe1d84a6cf1f598e50383f2be1071
bella meng <dancingm1998@gmail.com>
2021-04-23 10:59:07 +0800

    print tx hash

--- example/example.cpp
+++ example/example.cpp
@@ -5,33 +5,6 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
-    Blockchain chain(argv[1]);
-
-    int64_t block_index = 0;
-    int64_t tx_index = 0;
-    int64_t inputCount = 0;
-    int64_t outputCount = 0;
-    
-    for (auto block : chain) {
-        RANGES_FOR(auto tx, block) {
-            inputCount += tx.inputCount();
-            outputCount += tx.outputCount();
-            if (1000 <= block_index < 1030) {
-                // check how to cout the uint256
-                cout << " tx_index: " << tx_index << "; tx_hash: " << tx.getHash().GetHex();
-            }
-            tx_index += 1;
-        }
-        block_index += 1;
-        if (block_index > 1030)
-            break;
-    }
-
-    std::cout << "Chain contains:" << std::endl;
-    std::cout << block_index + 1 << " blocks" << std::endl;
-    std::cout << tx_index + 1 << " transactions" << std::endl;
-    std::cout << inputCount << " inputs" << std::endl;
-    std::cout << outputCount << " outputs" << std::endl;
-
+    index_addresses(argc, argv);
     return 0;
 }
