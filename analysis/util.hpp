92980f59632c772312350f35972b9aca1f0933c0
bella meng <dancingm1998@gmail.com>
2021-04-23 17:05:31 +0800

    print tx hash

--- analysis/util.hpp
+++ analysis/util.hpp
@@ -1,2 +1,11 @@
 #include <blocksci/blocksci.hpp>
 #include <iostream>
+#include <blocksci/chain/algorithms.hpp>
+#include <blocksci/chain/transaction.hpp>
+#include <blocksci/chain/block.hpp>
+#include <blocksci/script.hpp>
+#include <blocksci/address/address.hpp>
+#include <blocksci/chain/transaction_range.hpp>
+#include <blocksci/heuristics/taint.hpp>
+
+#include <range/v3/all.hpp>
