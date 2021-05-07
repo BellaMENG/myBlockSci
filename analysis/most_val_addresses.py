e3bb1221ea93add36fb32f5b12ad1773dc940c73
bella meng <dancingm1998@gmail.com>
2021-05-07 17:47:26 +0800

    most_valuable_addresses() test

--- analysis/most_val_addresses.py
+++ analysis/most_val_addresses.py
@@ -1,4 +1,10 @@
 import blocksci
+import time
 
 chain = blocksci.Blockchain('/home/zmengaa/blocksci.config')
+print("start")
+start_time = time.process_time()
 addrs = chain.most_valuable_addresses()
+end_time = time.process_time()
+
+print("--- %s seconds ---" % (time.time() - start_time))
