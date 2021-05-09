2b2eb04766872d6064e3a1fe34090653742a0876
bella meng <dancingm1998@gmail.com>
2021-05-09 23:29:25 +0800

    test clustering

--- test/blockscipy/run_clustering.py
+++ test/blockscipy/run_clustering.py
@@ -4,7 +4,7 @@ import tempfile as tmpdir_factory
 
 chain = blocksci.Blockchain('/home/zmengaa/blocksci.config', 600000)
 
-tmpdir_factory = "/home/zmengaa/tmpdir"
+#tmpdir_factory = "/home/zmengaa/tmpdir"
 
 start_time = time.process_time()
 test_clustering_default_heuristic(chain, tmpdir_factory)
