15694dc815440d9a2b2e631ba106b6691d71be0d
bella meng <dancingm1998@gmail.com>
2021-05-09 23:37:22 +0800

    test clustering

--- test/blockscipy/run_clustering.py
+++ test/blockscipy/run_clustering.py
@@ -12,7 +12,7 @@ end_time = time.process_time()
 print("--- test default heuristics: %s seconds ---" % (time.time() - start_time))
 
 start_time = time.process_time()
-test_clustering_proxy_heuristic()
-end_time = time.process_time(chain, tmpdir_factory)
+test_clustering_proxy_heuristic(chain, tmpdir_factory)
+end_time = time.process_time()
 print("--- test default heuristics: %s seconds ---" % (time.time() - start_time))
 
