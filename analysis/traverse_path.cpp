d78589935bdc767ea2994f7e428a771be4ef5730
bella meng <dancingm1998@gmail.com>
2021-05-19 15:16:04 +0800

    test getAddressFromString

---
+++ analysis/traverse_path.cpp
@@ -0,0 +1,21 @@
+#include "util.hpp"
+
+
+void printAddressFromString(Blockchain &chain, string address) {
+    auto randomAddress = getAddressFromString(address, chain.getAccess());
+    if (randomAddress) {
+        cout << "scriptNum:" << randomAddress->scriptNum << endl;
+        cout << "type: " << randomAddress->type << endl;
+    }
+}
+
+
+int main(int argc, const char* argv[]) {
+    
+    string chain_fp = argv[1];
+    
+    Blockchain chain(chain_fp);
+    string addr = "3PXswrSTz7tW73BKFcU8GENGFtoagKUJP3";
+    printAddressFromString(chain, addr);
+    return 0;
+}
