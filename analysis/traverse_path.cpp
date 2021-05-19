4589eaf966a2dd094d33660b79c507d64e69d029
bella meng <dancingm1998@gmail.com>
2021-05-19 16:38:18 +0800

    test getAddressFromString

--- analysis/traverse_path.cpp
+++ analysis/traverse_path.cpp
@@ -16,7 +16,8 @@ void printOutputs(Blockchain &chain, string address) {
     if (randomAddress) {
         RANGES_FOR(auto out, randomAddress->getOutputs()) {
             Address outAddr = out.getAddress();
-            cout << outAddr.toString() << endl;
+            if (outAddr != *randomAddress)
+                cout << outAddr.toString() << endl;
         }
     }
 }
